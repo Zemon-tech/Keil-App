a41cc389ee9bcb0c52edb5f6ec036c9c35b35785
ShivangKandoi <shivangkandoi006@gmail.com>
2026-02-21 12:00:28 +0530

    added placeholder image and update UI of SignUP page

--- frontend/src/components/auth/LoginForm.tsx
+++ frontend/src/components/auth/LoginForm.tsx
@@ -97,7 +97,7 @@ export function LoginForm({
           </form>
           <div className="bg-muted relative hidden md:block">
             <img
-              src="/placeholder.svg"
+              src="/placeholder.png"
               alt="Image"
               className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
             />
